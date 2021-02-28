ae3738a19e12d9dcd92999121abaebdfb63d20ca
Hoyoung Jung <honux77@gmail.com>
2021-02-28 12:07:34 +0900

    Add parameter to welcome page
    - name, age를 받아와서 콘트롤러에서 model을 이용 템플릿에 보내줌

--- src/main/java/com/codessquad/qna/web/WelcomeController.java
+++ src/main/java/com/codessquad/qna/web/WelcomeController.java
@@ -3,6 +3,7 @@ package com.codessquad.qna.web;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -11,8 +12,10 @@ public class WelcomeController {
     Logger logger = LoggerFactory.getLogger(WelcomeController.class);
 
     @GetMapping("/hello")
-    public String welcome() {
+    public String welcome(String name, int age, Model model) {
         logger.info("GET /hello");
+        model.addAttribute("name", name);
+        model.addAttribute("age", age);
         return "welcome";
     }
 }
