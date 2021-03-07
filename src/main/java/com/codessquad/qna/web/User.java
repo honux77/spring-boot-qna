efd400464025a1656118b48f9c87378068d36da0
Hoyoung Jung <honux77@gmail.com>
2021-03-08 02:29:07 +0900

    Fix templates of user lists
    - 경로 수정
    - http request test file 추가
    - 기타

--- src/main/java/com/codessquad/qna/web/User.java
+++ src/main/java/com/codessquad/qna/web/User.java
@@ -1,16 +1,26 @@
 package com.codessquad.qna.web;
 
 public class User {
-    private Long id;
     private String email;
     private String name;
     private String password;
 
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
     @Override
     public String toString() {
         return "User{" +
-                "id=" + id +
-                ", email='" + email + '\'' +
+                "email='" + email + '\'' +
                 ", name='" + name + '\'' +
                 ", password='" + password + '\'' +
                 '}';
--- src/main/java/com/codessquad/qna/web/UserController.java
+++ src/main/java/com/codessquad/qna/web/UserController.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -11,9 +12,15 @@ public class UserController {
 
     Logger logger = LoggerFactory.getLogger(UserController.class);
 
+    @GetMapping("/users")
+    public String users() {
+        logger.debug("List users");
+        return "users/list";
+    }
+
     @PostMapping("/users")
     public String create(User user, Model model) {
         logger.debug("User info: " + user);
-        return "redirect:/";
+        return "redirect:/users";
     }
 }
