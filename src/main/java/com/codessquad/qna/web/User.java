cd3599911bcff5c8184230efb206afdedae40e15
Hoyoung Jung <honux77@gmail.com>
2021-03-08 02:51:02 +0900

    Add users to UserController
    - List users 추가
    - 사용자 추가 로직 구현

--- src/main/java/com/codessquad/qna/web/User.java
+++ src/main/java/com/codessquad/qna/web/User.java
@@ -1,9 +1,16 @@
 package com.codessquad.qna.web;
 
+import java.util.Objects;
+
 public class User {
+    private Integer id;
     private String email;
     private String name;
     private String password;
+
+    public Integer getId() {
+        return id;
+    }
 
     public void setEmail(String email) {
         this.email = email;
@@ -15,6 +22,31 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return email.equals(user.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(email);
     }
 
     @Override
--- src/main/java/com/codessquad/qna/web/UserController.java
+++ src/main/java/com/codessquad/qna/web/UserController.java
@@ -7,20 +7,47 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class UserController {
 
     Logger logger = LoggerFactory.getLogger(UserController.class);
 
+    private List<User> users = new ArrayList<>();
+
     @GetMapping("/users")
-    public String users() {
-        logger.debug("List users");
+    public String users(Model model) {
+        logger.debug("List users, total users = " + users.size());
+        model.addAttribute("users", users);
         return "users/list";
     }
 
     @PostMapping("/users")
     public String create(User user, Model model) {
         logger.debug("User info: " + user);
+
+        if (!validate(user)) {
+            logger.debug("User already exist: " + user);
+            return "redirect:/users/form.html?error=FailToCreateUser";
+        }
+
+        addUser(user);
         return "redirect:/users";
+    }
+
+    private void addUser(User user) {
+        user.setId(users.size() + 1);
+        users.add(user);
+    }
+
+    private boolean validate(User newUser) {
+        for (User user : users) {
+            if (user.equals(newUser)) {
+                return false;
+            }
+        }
+        return true;
     }
 }
