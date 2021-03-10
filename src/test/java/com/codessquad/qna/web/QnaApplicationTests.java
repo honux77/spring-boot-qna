0aca52378b10f69629e9e97309b0abee57701603
Hoyoung Jung <honux77@gmail.com>
2021-03-10 22:07:53 +0900

    Update index page
    - 템플릿 내용 수정

--- src/test/java/com/codessquad/qna/web/QnaApplicationTests.java
+++ src/test/java/com/codessquad/qna/web/QnaApplicationTests.java
@@ -3,22 +3,18 @@ package com.codessquad.qna.web;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-
-@WebMvcTest(WelcomeController.class)
+@WebMvcTest
 class WelcomeControllerTest {
-
 	@Autowired
 	private MockMvc mvc;
 
 	@Test
 	void contextLoads() throws Exception {
-		mvc.perform(get("/hello?name=honux&age=20")).andExpect(status().isOk());
+		mvc.perform(get("/")).andExpect(status().isOk());
 	}
-
 }
